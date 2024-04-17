93bc3659e698a4b2a89361e943034f05cd08f877
Shubham Singhal <shubham.singhal@rsk-bsl.com>
2024-04-17 11:31:49 +0530

    Added api description for arable and soil api

--- nmp-api/src/vendors/rb209/soil/soil.controller.ts
+++ nmp-api/src/vendors/rb209/soil/soil.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Req } from '@nestjs/common';
 import { RB209SoilService } from './soil.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('RB209 Soil')
 @Controller('vendors/rb209/Soil')
@@ -8,6 +8,7 @@ export class RB209SoilController {
   constructor(private readonly service: RB209SoilService) {}
 
   @Get('/SoilTypes')
+  @ApiOperation({ summary: 'The full list of available Soil Types' })
   async getSoilTypes(@Req() req: Request) {
     const url = req.url.split('/rb209')[1];
     return await this.service.getData(url);
