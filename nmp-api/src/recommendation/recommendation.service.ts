8deac2760b23a3f0b8471af32db064543dcfb9e0
shubham.singhal <shubham.singhal@rsk-bsl.com>
2024-05-23 16:59:37 +0530

    Get Recommendations request

--- nmp-api/src/recommendation/recommendation.service.ts
+++ nmp-api/src/recommendation/recommendation.service.ts
@@ -5,6 +5,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { RecommendationEntity } from '@db/entity/recommendation.entity';
 import { ApiDataResponseType } from '@shared/base.response';
 import { BaseService } from '@src/base/base.service';
+import { RecommendationCommentEntity } from '@db/entity/recommendation-comment.entity';
 
 @Injectable()
 export class RecommendationService extends BaseService<
@@ -14,14 +15,16 @@ export class RecommendationService extends BaseService<
   constructor(
     @InjectRepository(RecommendationEntity)
     protected readonly repository: Repository<RecommendationEntity>,
+    @InjectRepository(RecommendationCommentEntity)
+    protected readonly recommendationCommentRepository: Repository<RecommendationCommentEntity>,
     protected readonly entityManager: EntityManager,
   ) {
     super(repository, entityManager);
   }
 
   async getNutrientsRecommendationsForField(
-    fieldId,
-    harvestYear,
+    fieldId: number,
+    harvestYear: number,
   ): Promise<RecommendationEntity[]> {
     try {
       const storedProcedure =
@@ -30,7 +33,29 @@ export class RecommendationService extends BaseService<
         fieldId,
         harvestYear,
       ]);
-      return recommendations;
+      const mappedRecommendations = recommendations.map((r) => {
+        const data = { Crop: {}, Recommendation: {} };
+        Object.keys(r).forEach((recDataKey) => {
+          if (recDataKey.startsWith('Crop_'))
+            data.Crop[recDataKey.slice(5)] = r[recDataKey];
+          else if (recDataKey.startsWith('Recommendation_'))
+            data.Recommendation[recDataKey.slice(15)] = r[recDataKey];
+        });
+        return data;
+      });
+
+      const dataWithComments = await Promise.all(
+        mappedRecommendations.map(async (r) => {
+          const comments = await this.recommendationCommentRepository.find({
+            where: {
+              RecommendationID: r.Recommendation.ID,
+            },
+          });
+
+          return { ...r, RecommendationComments: comments };
+        }),
+      );
+      return dataWithComments;
     } catch (error) {
       console.error('Error while fetching join data:', error);
       throw error;
