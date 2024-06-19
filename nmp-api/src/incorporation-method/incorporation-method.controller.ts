c73b376c98ac2cb1f5a97af446deef6c651cedb3
Aman Munet <aman.munet@RSK-BSL.COM>
2024-06-19 14:30:01 +0530

    Incorporation Delay and Incorporation Method API Refinement and Updated TestCases

--- nmp-api/src/incorporation-method/incorporation-method.controller.ts
+++ nmp-api/src/incorporation-method/incorporation-method.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { IncorporationMethodService } from './incorporation-method.service';
-import { ApiOperation, ApiSecurity, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiQuery, ApiSecurity, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Incorporation Methods')
 @Controller('incorporation-methods')
@@ -10,15 +10,26 @@ export class IncorporationMethodController {
     private readonly incorporationMethodService: IncorporationMethodService,
   ) {}
 
-  @Get(':appId')
+  @Get('/:appId')
   @ApiOperation({
     summary: 'Get list of Incorporation Methods by Application Id',
   })
-  async getIncorporationMethods(@Param('appId') appId: number) {
-    const data =
-      await this.incorporationMethodService.getIncorporationMethodsByAppId(
-        appId,
-      );
+  @ApiQuery({
+    name: 'fieldType',
+    description:
+      '1 for ApplicableForGrass, 2 for ApplicableForArableAndHorticulture',
+  })
+  async getIncorporationMethods(
+    @Query('fieldType', new ParseIntPipe({ optional: false }))
+    fieldType: number,
+    @Query('applicableFor') applicableFor: string,
+    @Param('appId') appId: number,
+  ) {
+    const data = await this.incorporationMethodService.getIncorporationMethods(
+      fieldType,
+      applicableFor,
+      appId,
+    );
     return { IncorporationMethods: data };
   }
 }
