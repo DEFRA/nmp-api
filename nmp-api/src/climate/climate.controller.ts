7159b0976ad13224d3f18e4ff8a75e9cf1c88b9b
shrey2692 <shreyash.chaturvedi@rsk-bsl.com>
2024-07-05 14:59:14 +0530

    minor changes

--- nmp-api/src/climate/climate.controller.ts
+++ nmp-api/src/climate/climate.controller.ts
@@ -29,14 +29,6 @@ export class ClimateController {
     name: 'postcode',
     description: 'First Half of Postcode, e.g., AB12',
   })
-  @ApiQuery({
-    name: 'startDate',
-    description: 'Start date , e.g., 2024-01-01',
-  })
-  @ApiQuery({
-    name: 'endDate',
-    description: 'End date , e.g., 2024-12-31',
-  })
   @ApiOperation({ summary: 'Get total rainfall by postcode and date range' })
   async getTotalRainfallByPostcodeAndDate(
     @Query('postcode') postcode: string,
