0d1212af6027ab46eaaa33871e8b7961d4e1ad73
AmanMunetRsk <aman.munet@rsk-bsl.com>
2024-07-31 10:53:57 +0530

    Refined Inorganic Manure Duration Entity

--- nmp-api/test/mocked-data/inorganicManureDuration.ts
+++ nmp-api/test/mocked-data/inorganicManureDuration.ts
@@ -1,7 +1,5 @@
 export const inorganicManureDurationReqBody = {
   Name: 'Mid June to July',
-  StartDate: 16,
-  StartMonth: 6,
-  EndDate: 31,
-  EndMonth: 7,
+  ApplicationDate: 7,
+  ApplicationMonth: 7,
 };
