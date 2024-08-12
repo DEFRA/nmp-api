278fcd25c4a8d8c0b82a44f2f747567264c42ce2
AbhishekRSKOfficial <165009146+AbhishekRSKOfficial@users.noreply.github.com>
2024-08-12 20:37:43 +0530

    Update deploy.bat

--- nmp-api/scripts/deploy.bat
+++ nmp-api/scripts/deploy.bat
@@ -1,4 +1,21 @@
+@echo off
 
-npm install --global yarn
-yarn --version
-yarn install
+:: Install npm packages
+echo Installing npm packages...
+cd "%DEPLOYMENT_TARGET%"
+npm install --force
+
+:: Update npm
+echo Updating npm to the latest version...
+npm install -g npm
+
+:: DEPLOYMENT_SOURCE=C:\home
+:: C:\home\site\wwwroot
+
+:: Proceed with the standard Azure App Service deployment
+echo Running the standard Azure App Service deployment script...
+IF EXIST "%DEPLOYMENT_TARGET%\deploy.cmd" (
+    call "%DEPLOYMENT_TARGET%\deploy.cmd"
+) ELSE (
+    echo Default deployment script not found. Skipping...
+)
