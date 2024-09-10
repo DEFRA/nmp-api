ea05ef39d72fa1b98caca0ca76d0dbe5e3acd7c9
shrey2692 <shreyash.chaturvedi@rsk-bsl.com>
2024-09-10 18:11:29 +0530

    remove address lookup from health

--- nmp-api/src/app.service.ts
+++ nmp-api/src/app.service.ts
@@ -14,13 +14,13 @@ export class AppService {
   async health(): Promise<any> {
     try {
       const dbHealth = await this.checkDatabaseHealth();
-      const rb209Health = await this.checkRB209Health();
-      const addressLookupHealth = await this.checkAddressLookupHealth();
+      // const rb209Health = await this.checkRB209Health();
+      // const addressLookupHealth = await this.checkAddressLookupHealth();
 
       return {
         nmp_api: dbHealth,
-        rb209_api: rb209Health,
-        addressLookup_api: addressLookupHealth,
+        // rb209_api: rb209Health,
+        // addressLookup_api: addressLookupHealth,
       };
     } catch (error) {
       console.error('Error during health check:', error);
