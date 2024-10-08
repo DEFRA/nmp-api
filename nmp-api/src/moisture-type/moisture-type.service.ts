6b10b53726da7db90948f365e0468902737970fb
shrey2692 <shreyash.chaturvedi@rsk-bsl.com>
2024-10-08 13:42:11 +0530

    moisture default

--- nmp-api/src/moisture-type/moisture-type.service.ts
+++ nmp-api/src/moisture-type/moisture-type.service.ts
@@ -4,6 +4,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { ApiDataResponseType } from '@shared/base.response';
 import { validateISODateFormat } from '@shared/dateValidate';
 import { BaseService } from '@src/base/base.service';
+import { MannerMoistureTypesService } from '@src/vendors/manner/moisture-types/moisture-types.service';
 import { Repository, EntityManager } from 'typeorm';
 
 @Injectable()
@@ -15,20 +16,33 @@ export class MoistureTypeService extends BaseService<
     @InjectRepository(MoistureTypeEntity)
     protected readonly repository: Repository<MoistureTypeEntity>,
     protected readonly entityManager: EntityManager,
+    protected readonly moistureMannerService: MannerMoistureTypesService,
   ) {
     super(repository, entityManager);
   }
 
   async getDefaultSoilMoistureType(applicationDate: string) {
     const date = validateISODateFormat(applicationDate);
+    //calling manner api
+    const moisturTypeList =
+      await this.moistureMannerService.getData('moisture-types');
 
-    const month = date.getMonth() + 1;
-    const soilMoistureType =
-      month == 5 || month == 6 || month == 7 ? 'Dry' : 'Moist';
+    if (moisturTypeList.success && moisturTypeList.data) {
+      // Get the month from the application date
+      const month = date.getMonth() + 1;
 
-    const moistureType = await this.repository.findOneBy({
-      Name: soilMoistureType,
-    });
-    return moistureType;
+      // Determine soil moisture type based on the month
+      const soilMoistureType =
+        month === 5 || month === 6 || month === 7 ? 'Dry' : 'Moist';
+
+      // Find the corresponding moisture type in the manner data
+      const moistureType = moisturTypeList.data.find(
+        (item: any) => item.name === soilMoistureType,
+      );
+
+      return moistureType;
+    } else {
+      throw new Error('Failed to fetch moisture types or data is unavailable');
+    }
   }
 }
