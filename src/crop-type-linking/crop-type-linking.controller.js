dd923e8c97bc0e5afdfd54a492096f41e421595c
shrey2692 <shreyash.chaturvedi@rsk-bsl.com>
2025-01-03 17:35:25 +0530

    CropTypeQuestions

--- src/crop-type-linking/crop-type-linking.controller.js
+++ src/crop-type-linking/crop-type-linking.controller.js
@@ -32,7 +32,7 @@ class CropTypeLinkingsController {
     const { cropTypeID } = this.#request.params;
     try {
       const record =
-        await this.#cropTypeLinkingsService.getCropTypeLinkingByCropTypeID(
+        await this.#cropTypeLinkingsService.getCropInfoQuestionsByCropTypeID(
           cropTypeID
         );
       if (!record) {
@@ -40,7 +40,7 @@ class CropTypeLinkingsController {
           "No CropTypeLinking Data found based on CropTypeID"
         );
       }
-      return this.#h.response({ CropTypeLinking: record });
+      return this.#h.response({ CropTypeQuestion: record });
     } catch (error) {
       return this.#h.response({ error });
     }
