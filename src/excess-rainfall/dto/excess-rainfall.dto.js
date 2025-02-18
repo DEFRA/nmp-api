5f8f37f0ba74f7375d10dd0e1a3005f2dee7b4ff
shrey2692 <shreyash.chaturvedi@rsk-bsl.com>
2025-02-18 18:52:04 +0530

    excessrainfall

--- src/excess-rainfall/dto/excess-rainfall.dto.js
+++ src/excess-rainfall/dto/excess-rainfall.dto.js
@@ -1,12 +1,14 @@
 const Joi = require("joi");
 
 const ExcessRainfallDto = Joi.object({
-  ExcessRainfall: Joi.number().integer().default(0).allow(null), 
-  WinterRainfall: Joi.number().integer().allow(null), 
-  CreatedOn: Joi.date().iso().allow(null).default(null), 
-  CreatedByID: Joi.number().integer().allow(null).default(null), 
-  ModifiedOn: Joi.date().iso().allow(null).default(null), 
-  ModifiedByID: Joi.number().integer().allow(null).default(null), 
+  FarmID: Joi.number().integer().required().default(null),
+  Year: Joi.number().integer().required().default(null),
+  ExcessRainfall: Joi.number().integer().default(0).allow(null),
+  WinterRainfall: Joi.number().integer().allow(null),
+  CreatedOn: Joi.date().iso().allow(null).default(null),
+  CreatedByID: Joi.number().integer().allow(null).default(null),
+  ModifiedOn: Joi.date().iso().allow(null).default(null),
+  ModifiedByID: Joi.number().integer().allow(null).default(null),
 });
 
 module.exports = {
