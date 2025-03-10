9e50b7e103e528a39ec55f818cea239d45a21e2b
shrey2692 <shreyash.chaturvedi@rsk-bsl.com>
2025-03-10 17:15:35 +0530

    some minor changes

--- src/sns-analysis/sns-analysis.controller.js
+++ src/sns-analysis/sns-analysis.controller.js
@@ -43,47 +43,6 @@ class SNSAnalysesController {
       return this.#h.response({ error });
     }
   }
-
-  async updateSNSAnalysis() {
-    const { snsAnalysisId } = this.#request.params;
-    const updatedSNSAnalysisData = this.#request.payload.SNSAnalysis;
-    const userId = this.#request.userId;
-    const pKBalanceData = this.#request.payload.PKBalance;
-
-    try {
-      const data = await this.#snsAnalysisService.updateSNSAnalysis(
-        updatedSNSAnalysisData,
-        userId,
-        parseInt(snsAnalysisId),
-        pKBalanceData,
-        this.#request
-      );
-
-      return this.#h.response({ data });
-    } catch (error) {
-      return this.#h.response({ error });
-    }
-  }
-
-  async deleteSNSAnalysisById() {
-    const { snsAnalysisId } = this.#request.params;
-    const userId = this.#request.userId;
-    try {
-      const result = await this.#snsAnalysisService.deleteSNSAnalysis(
-        snsAnalysisId,
-        userId,
-        this.#request
-      );
-      if (result?.affectedRows === 0) {
-        throw boom.notFound(`SNS analysis with ID ${snsAnalysisId} not found.`);
-      }
-      return this.#h.response({
-        message: "SNS analysis deleted successfully.",
-      });
-    } catch (error) {
-      return this.#h.response({ error: error.message });
-    }
-  }
 }
 
 module.exports = { SNSAnalysesController };
