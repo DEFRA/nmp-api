446127cc35d8056b3b3b28d07d4f2e13c047260d
PoojaTaneja1999 <pooja.taneja@RSK-BSL.com>
2026-02-19 15:28:53 +0530

    little changes

---
+++ src/db/entity/farms-nvz.entity.js
@@ -0,0 +1,56 @@
+const { EntitySchema } = require("typeorm");
+
+const FarmsNVZEntity = new EntitySchema({
+  name: "FarmsNVZ",
+  tableName: "FarmsNVZ",
+  columns: {
+    ID: {
+      type: "int",
+      primary: true,
+      generated: true,
+      generationStrategy: "identity",
+    },
+    FarmID: {
+      type: "int",
+      nullable: false,
+    },
+    NVZProgrammeID: {
+      type: "int",
+      nullable: false,
+    },
+    NVZProgrammeName: {
+      type: "nvarchar",
+      length: 128,
+    },    
+    CreatedOn: {
+      type: "datetime2",
+      nullable: true,
+      precision: 7,
+      default: "GETDATE()",
+    },
+    CreatedByID: {
+      type: "int",
+      nullable: true,
+    },
+    ModifiedOn: {
+      type: "datetime2",
+      nullable: true,
+      precision: 7,
+      default: "GETDATE()",
+    },
+    ModifiedByID: {
+      type: "int",
+      nullable: true,
+    },
+  },
+  relations: {
+    Farm: {
+      type: "one-to-many",
+      target: "Farm",
+      joinColumn: { name: "FarmID" },
+      inverseSide: "FarmsNVZ",
+    },
+  },
+});
+
+module.exports = { FarmsNVZEntity };
