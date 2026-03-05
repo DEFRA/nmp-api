f33078d462818224223f8ef4aaa9acac893d9fe1
shrey2692 <shreyash.chaturvedi@rsk-bsl.com>
2026-03-05 22:34:10 +0530

    duplicate code removed

--- src/soil-analyses-method/soil-analyses-method.route.js
+++ src/soil-analyses-method/soil-analyses-method.route.js
@@ -1,7 +1,23 @@
 const Joi = require("joi");
 const { formatErrorResponse } = require("../interceptor/responseFormatter");
-const { SoilAnalysesMethodController } = require("./soil-analyses-method.controller");
+const {
+  SoilAnalysesMethodController,
+} = require("./soil-analyses-method.controller");
 const { StatusCodeMapper } = require("../constants/http-status-codes-mapper");
+
+const handleValidationError = (request, h, err) =>
+  h
+    .response(
+      formatErrorResponse({
+        source: { error: err },
+        request,
+      }),
+    )
+    .code(StatusCodeMapper.BAD_REQUEST)
+    .takeover();
+
+const getController = (request, h) =>
+  new SoilAnalysesMethodController(request, h);
 
 module.exports = [
   {
@@ -11,8 +27,8 @@ module.exports = [
       tags: ["api", "Soil Analyses Method"],
       description: "Get all Soil Analyses Method",
     },
-    handler: async (request, h) => {
-      const controller = new SoilAnalysesMethodController(request, h);
+    handler: (request, h) => {
+      const controller = getController(request, h);
       return controller.getAllSoilAnalysesMethods();
     },
   },
@@ -26,21 +42,11 @@ module.exports = [
         params: Joi.object({
           id: Joi.number().integer().required(),
         }),
-        failAction: (request, h, err) => {
-          return h
-            .response(
-              formatErrorResponse({
-                source: { error: err },
-                request,
-              }),
-            )
-            .code(StatusCodeMapper.BAD_REQUEST)
-            .takeover();
-        },
+        failAction: handleValidationError,
       },
     },
-    handler: async (request, h) => {
-      const controller = new SoilAnalysesMethodController(request, h);
+    handler: (request, h) => {
+      const controller = getController(request, h);
       return controller.getSoilAnalysesMethodById();
     },
   },
