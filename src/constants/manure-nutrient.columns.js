0e133440298964458003a314d747ac1d49a97960
shrey2692 <shreyash.chaturvedi@rsk-bsl.com>
2026-04-15 16:02:29 +0530

    optimize Manure Type Entity Code

---
+++ src/constants/manure-nutrient.columns.js
@@ -0,0 +1,49 @@
+const manureNutrientColumns = {
+  DryMatter: {
+    type: "decimal",
+    precision: 18,
+    scale: 2,
+  },
+  TotalN: {
+    type: "decimal",
+    precision: 18,
+    scale: 2,
+  },
+  NH4N: {
+    type: "decimal",
+    precision: 18,
+    scale: 2,
+  },
+  Uric: {
+    type: "decimal",
+    precision: 18,
+    scale: 2,
+  },
+  NO3N: {
+    type: "decimal",
+    precision: 18,
+    scale: 2,
+  },
+  P2O5: {
+    type: "decimal",
+    precision: 18,
+    scale: 2,
+  },
+  K2O: {
+    type: "decimal",
+    precision: 18,
+    scale: 2,
+  },
+  SO3: {
+    type: "decimal",
+    precision: 18,
+    scale: 2,
+  },
+  MgO: {
+    type: "decimal",
+    precision: 18,
+    scale: 2,
+  },
+};
+
+module.exports = { manureNutrientColumns };
