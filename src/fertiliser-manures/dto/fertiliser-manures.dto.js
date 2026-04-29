a23ae54f3bf57f6d02ff5494d06283344a5c7c19
shrey2692 <shreyash.chaturvedi@rsk-bsl.com>
2026-04-29 14:44:33 +0530

    spelling correction

--- src/fertiliser-manures/dto/fertiliser-manures.dto.js
+++ src/fertiliser-manures/dto/fertiliser-manures.dto.js
@@ -1,27 +1,27 @@
 const Joi = require("joi");
 const { WarningMessageItemSchema } = require("../../organic-manure/dto/organic-manure.dto");
-const precsionThree = 3;
+const precisionThree = 3;
 const FertiliserManureDto = Joi.object({
   ID: Joi.number().integer().allow(null).optional().default(null),
   ManagementPeriodID: Joi.number().integer().required(),
   ApplicationDate: Joi.date().iso().required(),
-  ApplicationRate: Joi.number().precision(precsionThree).required(),
+  ApplicationRate: Joi.number().precision(precisionThree).required(),
   Confirm: Joi.boolean().required(),
-  N: Joi.number().precision(precsionThree).required(),
-  P2O5: Joi.number().precision(precsionThree).required(),
-  K2O: Joi.number().precision(precsionThree).required(),
-  MgO: Joi.number().precision(precsionThree).required(),
-  SOprecsionThree: Joi.number().precision(precsionThree).required(),
-  Na2O: Joi.number().precision(precsionThree).required(),
-  NFertAnalysisPercent: Joi.number().precision(precsionThree).required(),
-  P2O5FertAnalysisPercent: Joi.number().precision(precsionThree).required(),
-  K2OFertAnalysisPercent: Joi.number().precision(precsionThree).required(),
-  MgOFertAnalysisPercent: Joi.number().precision(precsionThree).required(),
-  SOprecsionThreeFertAnalysisPercent: Joi.number().precision(precsionThree).required(),
-  Na2OFertAnalysisPercent: Joi.number().precision(precsionThree).required(),
-  Lime: Joi.number().precision(precsionThree).required(),
-  NH4N: Joi.number().precision(precsionThree).required(),
-  NOprecsionThreeN: Joi.number().precision(precsionThree).required(),
+  N: Joi.number().precision(precisionThree).required(),
+  P2O5: Joi.number().precision(precisionThree).required(),
+  K2O: Joi.number().precision(precisionThree).required(),
+  MgO: Joi.number().precision(precisionThree).required(),
+  SOprecisionThree: Joi.number().precision(precisionThree).required(),
+  Na2O: Joi.number().precision(precisionThree).required(),
+  NFertAnalysisPercent: Joi.number().precision(precisionThree).required(),
+  P2O5FertAnalysisPercent: Joi.number().precision(precisionThree).required(),
+  K2OFertAnalysisPercent: Joi.number().precision(precisionThree).required(),
+  MgOFertAnalysisPercent: Joi.number().precision(precisionThree).required(),
+  SOprecisionThreeFertAnalysisPercent: Joi.number().precision(precisionThree).required(),
+  Na2OFertAnalysisPercent: Joi.number().precision(precisionThree).required(),
+  Lime: Joi.number().precision(precisionThree).required(),
+  NH4N: Joi.number().precision(precisionThree).required(),
+  NOprecisionThreeN: Joi.number().precision(precisionThree).required(),
 }).required();
 
 
