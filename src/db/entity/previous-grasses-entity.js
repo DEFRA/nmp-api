f37d1f1cf4276cfc7a485d455097928b60ff70a4
shrey2692 <shreyash.chaturvedi@rsk-bsl.com>
2026-05-04 16:39:43 +0530

    reduce some copying

--- src/db/entity/previous-grasses-entity.js
+++ src/db/entity/previous-grasses-entity.js
@@ -1,7 +1,38 @@
 const { EntitySchema } = require("typeorm");
 const { auditColumns } = require("../../constants/audits-columns");
 const { RELATION_TYPES } = require("../../constants/relations-mapper");
-
+const relations = {
+  Fields: {
+    type: RELATION_TYPES.MANY_TO_ONE,
+    target: "Field",
+    joinColumn: { name: "FieldID" },
+    inverseSide: "PreviousGrasses",
+  },
+  GrassManagementOptions: {
+    type: RELATION_TYPES.MANY_TO_ONE,
+    target: "GrassManagementOptions",
+    joinColumn: { name: "GrassManagementOptionID" },
+    inverseSide: "PreviousGrasses",
+  },
+  SoilNitrogenSupplyItems: {
+    type: RELATION_TYPES.MANY_TO_ONE,
+    target: "SoilNitrogenSupplyItems",
+    joinColumn: { name: "SoilNitrogenSupplyItemID" },
+    inverseSide: "PreviousGrasses",
+  },
+  CreatedByUser: {
+    type: RELATION_TYPES.MANY_TO_ONE,
+    target: "User",
+    joinColumn: { name: "CreatedByID" },
+    inverseSide: "CreatedPreviousGrasses",
+  },
+  ModifiedByUser: {
+    type: RELATION_TYPES.MANY_TO_ONE,
+    target: "User",
+    joinColumn: { name: "ModifiedByID" },
+    inverseSide: "ModifiedPreviousGrasses",
+  },
+};
 const PreviousGrassesEntity = new EntitySchema({
   name: "PreviousGrasses",
   tableName: "PreviousGrasses",
@@ -40,38 +71,7 @@ const PreviousGrassesEntity = new EntitySchema({
     },
     ...auditColumns,
   },
-  relations: {
-    Fields: {
-      type: RELATION_TYPES.MANY_TO_ONE,
-      target: "Field",
-      joinColumn: { name: "FieldID" },
-      inverseSide: "PreviousGrasses",
-    },
-    GrassManagementOptions: {
-      type: RELATION_TYPES.MANY_TO_ONE,
-      target: "GrassManagementOptions",
-      joinColumn: { name: "GrassManagementOptionID" },
-      inverseSide: "PreviousGrasses",
-    },
-    SoilNitrogenSupplyItems: {
-      type: RELATION_TYPES.MANY_TO_ONE,
-      target: "SoilNitrogenSupplyItems",
-      joinColumn: { name: "SoilNitrogenSupplyItemID" },
-      inverseSide: "PreviousGrasses",
-    },
-    CreatedByUser: {
-      type: RELATION_TYPES.MANY_TO_ONE,
-      target: "User",
-      joinColumn: { name: "CreatedByID" },
-      inverseSide: "CreatedPreviousGrasses",
-    },
-    ModifiedByUser: {
-      type: RELATION_TYPES.MANY_TO_ONE,
-      target: "User",
-      joinColumn: { name: "ModifiedByID" },
-      inverseSide: "ModifiedPreviousGrasses",
-    },
-  },
+  relations: relations
 });
 
 module.exports = { PreviousGrassesEntity };
