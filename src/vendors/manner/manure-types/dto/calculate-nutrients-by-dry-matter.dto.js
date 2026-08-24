5af0b1e03baa281082910f034305e71d769e8ef3
shrey2692 <shreyash.chaturvedi@rsk-bsl.com>
2026-08-24 17:40:22 +0530

    remove duplicate issue

--- src/vendors/manner/manure-types/dto/calculate-nutrients-by-dry-matter.dto.js
+++ src/vendors/manner/manure-types/dto/calculate-nutrients-by-dry-matter.dto.js
@@ -2,6 +2,24 @@
 
 const Joi = require("joi");
 
+const nutrientFieldDescriptions = {
+  totalN: "Total nitrogen in the manure",
+  nH4N: "NH4N content in the manure",
+  uric: "Uric acid content in the manure",
+  nO3N: "NO3N content in the manure",
+  p2O5: "P2O5 content in the manure",
+  k2O: "K2O content in the manure",
+  sO3: "SO3 content in the manure",
+  mgO: "MgO content in the manure",
+};
+
+const NutrientFieldsDto = Object.fromEntries(
+  Object.entries(nutrientFieldDescriptions).map(([fieldName, description]) => [
+    fieldName,
+    Joi.number().required().description(description),
+  ]),
+);
+
 const CalculateNutrientsByDryMatterDto = Joi.object({
   id: Joi.number().integer().required().description("ID of the manure"),
 
@@ -9,21 +27,7 @@ const CalculateNutrientsByDryMatterDto = Joi.object({
     .required()
     .description("Dry matter percentage of the manure"),
 
-  totalN: Joi.number().required().description("Total nitrogen in the manure"),
-
-  nH4N: Joi.number().required().description("NH4N content in the manure"),
-
-  uric: Joi.number().required().description("Uric acid content in the manure"),
-
-  nO3N: Joi.number().required().description("NO3N content in the manure"),
-
-  p2O5: Joi.number().required().description("P2O5 content in the manure"),
-
-  k2O: Joi.number().required().description("K2O content in the manure"),
-
-  sO3: Joi.number().required().description("SO3 content in the manure"),
-
-  mgO: Joi.number().required().description("MgO content in the manure"),
+  ...NutrientFieldsDto,
 }).required();
 
 module.exports = {
